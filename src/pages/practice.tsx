a3a05e7a15ce37408537cfa1dd21a4d0659fbe81
hiroki-kitamura <h.kitamura410@gmail.com>
2020-08-18 02:07:26 +0900

    もくもくの内容を一部書く

--- src/pages/practice.tsx
+++ src/pages/practice.tsx
@@ -1,7 +1,40 @@
 import * as React from 'react';
-import { useEffect, useReducer } from 'react';
+import { useState, useEffect, useReducer } from 'react';
+import styled from 'styled-Components'
+
+const CountUpButton = styled.button`
+  width: 100px;
+  height: 100px;
+  font-size: 24px;
+  text-align: center;
+  line-height: 100px;
+  border-radius: 10px;
+  border: 1px solid #333;
+`
+const Count = styled.p`
+  width: 100px;
+  height: 100px;
+  font-size: 24px;
+  text-align: center;
+  line-height: 100px;
+  border-radius: 10px;
+  border: 1px solid #333;
+`
 
 export const Practice = () => {
+  const [count, setCount] = useState(0)
+  const countUp = () => {
+    setCount(count + 1)
+  }
 
-  return <h1>practice</h1>
+  window.addEventListener('click', () => {
+    countUp()
+  })
+
+  return (
+    <div>
+      <CountUpButton>countUp</CountUpButton>
+      <Count>{count}</Count>
+    </div>
+  )
 }
